299a8eab83f345fe9f9b255cfb6ae9779d0a7329
Ondřej Příhoda <ondrej.prihoda@email.cz>
2023-12-07 22:35:23 +0100

    feat: add further logic separation

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,44 +1,82 @@
-const fetchDataForNextTwoMonths = async (sunResource, sunRest, tempResource, tempRest, windResource, windRest) => {
-	const currentDate = new Date();
-	const currentYear = 2021; // You may want to use currentDate.getFullYear() for dynamic current year
-	const currentMonth = currentDate.getMonth() + 1; // Months are 0-indexed, so add 1.
+import { useAuth0 } from '../react-auth0-spa';
+import { useRest } from '../utils/helpers';
 
-	const isLastMonthOfYear = currentMonth === 12;
+export const useSportData = (sportType) => {
+	const { apiSun, apiTemp, apiAir, apiEnergy } = useAuth0();
+	const [{ resource: precipResource, rest: precipRest }] = useRest('precipitation');
+	const [{ resource: tempResource, rest: tempRest }] = useRest('temp-avg');
+	const [{ resource: windResource, rest: windRest }] = useRest('speed');
 
-	// Construct the $or query for RestDB API
-	const orQuery = [
-		{
-			year: currentYear,
-			month: currentMonth,
-		},
-	];
+	const fetchDataForNextTwoMonths = async () => {
+		const currentDate = new Date();
+		// TODO use global constant for year
+		const currentYear = 2021;
+		const currentMonth = currentDate.getMonth() + 1; // Months are 0-indexed, so add 1.
 
-	if (isLastMonthOfYear === true) {
-		orQuery.push({
-			year: currentYear + 1,
-			month: 1,
-		});
-	} else {
-		orQuery.push({
-			year: currentYear,
-			month: currentMonth + 1,
-		});
-	}
+		const isLastMonthOfYear = currentMonth === 12;
 
-	const restQuery = {
-		$or: orQuery,
+		// Construct the $or query for RestDB API
+		const orQuery = [
+			{
+				year: currentYear,
+				month: currentMonth,
+			},
+		];
+
+		if (isLastMonthOfYear === true) {
+			orQuery.push({
+				year: currentYear + 1,
+				month: 1,
+			});
+		} else {
+			orQuery.push({
+				year: currentYear,
+				month: currentMonth + 1,
+			});
+		}
+
+		const restQuery = {
+			$or: orQuery,
+		};
+
+		// Make API requests for precipitation, temperature, and wind
+		const precipitationData = precipResource ? await precipResource.get(`/rest/${precipRest}?q=${JSON.stringify(restQuery)}`) : null;
+		const temperatureData = tempResource ? await tempResource.get(`/rest/${tempRest}?q=${JSON.stringify(restQuery)}`) : null;
+		const windData = windResource ? await windResource.get(`/rest/${windRest}?q=${JSON.stringify(restQuery)}`) : null;
+
+		return {
+			precipitation: precipitationData,
+			temperature: temperatureData,
+			wind: windData,
+		};
 	};
 
-	// Make API requests for sunshine, temperature, and wind
-	const sunshineData = sunResource ? await sunResource.get(`/rest/${sunRest}?q=${JSON.stringify(restQuery)}`) : null;
-	const temperatureData = tempResource ? await tempResource.get(`/rest/${tempRest}?q=${JSON.stringify(restQuery)}`) : null;
-	const windData = windResource ? await windResource.get(`/rest/${windRest}?q=${JSON.stringify(restQuery)}`) : null;
+	const filterDataForNext10Days = (data) => {
+		return data;
+		// Filter data for the next 10 days logic here
+		// TODO data format
+		// Date
+		// Day of the week
+		// Temp.
+		// Precip.
+		// Wind
+		// Rating
+	};
+
+	const rateEachDay = (data) => {
+		return data;
+		// TODO Pass sport type as well
+		// Rating logic based on sport type here
+	};
+
+	const fetchDataAndProcess = async () => {
+		const rawData = await fetchDataForNextTwoMonths();
+		const filteredData = filterDataForNext10Days(rawData);
+		const ratedData = rateEachDay(filteredData);
+		return ratedData;
+	};
 
 	return {
-		sunshine: sunshineData,
-		temperature: temperatureData,
-		wind: windData,
+		fetchDataAndProcess,
 	};
 };
-
-export default fetchDataForNextTwoMonths;
--- src/views/APITestPage.js
+++ src/views/APITestPage.js
@@ -1,50 +1,46 @@
-// APITestPage.js
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useRest } from '../utils/helpers';
-import fetchDataForNextTwoMonths from '../utils/apiUtils';
+import { useSportData } from '../utils/apiUtils';
 
 const dataFetched = (data) => ({
-  type: 'DATA_FETCHED',
-  payload: data,
+	type: 'DATA_FETCHED',
+	payload: data,
 });
 
 const APITestPage = () => {
-  const [{ resource: precipResource, rest: precipRest }] = useRest('precipitation');
-  const [{ resource: tempResource, rest: tempRest }] = useRest('temp-avg');
-  const [{ resource: windResource, rest: windRest }] = useRest('speed');
-  const dispatch = useDispatch();
-  const data = useSelector(({ exampleReducer }) => exampleReducer.data);
+	const dispatch = useDispatch();
+	const data = useSelector(({ exampleReducer }) => exampleReducer.data);
+	const { fetchDataAndProcess } = useSportData('football'); // Pass the sport type
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetchDataForNextTwoMonths(precipResource, precipRest, tempResource, tempRest, windResource, windRest);
-      dispatch(dataFetched(result));
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await fetchDataAndProcess();
+			dispatch(dataFetched(result));
+		};
 
-    fetchData();
-  }, [precipResource, precipRest, tempResource, tempRest, windResource, windRest, dispatch]);
+		fetchData();
+	}, [fetchDataAndProcess, dispatch]);
 
-  return (
-    <div>
-      <h1>Results Page</h1>
-      {data && (
-        <div>
-          <h2>Sunshine Data</h2>
-          {/* Render sunshine data */}
-          {/* Example: {data.sunshine.map(item => <div key={item.id}>{item.name}</div>)} */}
+	return (
+		<div>
+			<h1>Results Page</h1>
+			{data && (
+				<div>
+					<h2>Precipitation Data</h2>
+					{/* Render Precipitation data */}
+					{/* Example: {data.precipitation.map(item => <div key={item.id}>{item.name}</div>)} */}
 
-          <h2>Temperature Data</h2>
-          {/* Render temperature data */}
-          {/* Example: {data.temperature.map(item => <div key={item.id}>{item.name}</div>)} */}
+					<h2>Temperature Data</h2>
+					{/* Render temperature data */}
+					{/* Example: {data.temperature.map(item => <div key={item.id}>{item.name}</div>)} */}
 
-          <h2>Wind Data</h2>
-          {/* Render wind data */}
-          {/* Example: {data.wind.map(item => <div key={item.id}>{item.name}</div>)} */}
-        </div>
-      )}
-    </div>
-  );
+					<h2>Wind Data</h2>
+					{/* Render wind data */}
+					{/* Example: {data.wind.map(item => <div key={item.id}>{item.name}</div>)} */}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default APITestPage;
