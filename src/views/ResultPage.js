6afd34df6db677b466f14b5345967b861500c88d
Ondřej Příhoda <ondrej.prihoda@email.cz>
2023-12-10 12:52:09 +0100

    feat: integrate API & searchValues into ResultPage

--- src/views/ResultPage.js
+++ src/views/ResultPage.js
@@ -1,121 +1,53 @@
-import React, {useEffect} from 'react';
-import {makeStyles} from '@mui/styles';
-import {useLocation, useNavigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Forecast from '../components/forecast/Forecast';
 import TranslucentBox from '../components/TranslucentBox';
 import SearchForm from '../components/SearchForm';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { useSportData } from '../utils/apiUtils';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import LineGraph from '../components/graphs/LineGraph';
+import BarGraph from '../components/graphs/BarGraph';
+import { translate } from '../services/LanguageService';
 
 const useStyles = makeStyles((theme) => {
 	return {
 		root: {
 			display: 'block',
-			marginTop: '1em'
+			marginTop: '1em',
 		},
 		wrapper: {
 			display: 'flex',
 			justifyContent: 'center',
 			alignItems: 'center',
-			flexDirection: 'column'
+			flexDirection: 'column',
 		},
 		search: {
-			width: '75%'
+			width: '75%',
 		},
 		forecast: {
 			display: 'block',
-			width: '75%'
+			width: '75%',
+		},
+		graphs: {
+			display: 'block',
+			width: '75%',
 		},
 	};
 });
 
-const dailyData = [
-	{
-		date: new Date(2022, 11, 11),
-		day: 'monday',
-		temperature: 21,
-		precipitation: 3,
-		wind: 7,
-		rating: 3
-	},
-	{
-		date: new Date(2022, 11, 12),
-		day: 'tuesday',
-		temperature: 21,
-		precipitation: 3,
-		wind: 17,
-		rating: 4
-	},
-	{
-		date: new Date(2022, 11, 13),
-		day: 'wednesday',
-		temperature: 21,
-		precipitation: 3,
-		wind: 27,
-		rating: 1
-	},
-	{
-		date: new Date(2022, 11, 14),
-		day: 'thursday',
-		temperature: 21,
-		precipitation: 3,
-		wind: 7,
-		rating: 2
-	},
-	{
-		date: new Date(2022, 11, 15),
-		day: 'friday',
-		temperature: 21,
-		precipitation: 3,
-		wind: 87,
-		rating: 3
-	},
-	{
-		date: new Date(2022, 11, 16),
-		day: 'saturday',
-		temperature: 21,
-		precipitation: 3,
-		wind: 7,
-		rating: 3
-	},
-	{
-		date: new Date(2022, 11, 17),
-		day: 'sunday',
-		temperature: 21,
-		precipitation: 3,
-		wind: 7,
-		rating: 3
-	},
-	{
-		date: new Date(2022, 11, 18),
-		day: 'monday',
-		temperature: 21,
-		precipitation: 3,
-		wind: 7,
-		rating: 3
-	},
-	{
-		date: new Date(2022, 11, 19),
-		day: 'tuesday',
-		temperature: 21,
-		precipitation: 3,
-		wind: 7,
-		rating: 3
-	},
-	{
-		date: new Date(2022, 11, 20),
-		day: 'wednesday',
-		temperature: 21,
-		precipitation: 3,
-		wind: 7,
-		rating: 3
-	},
-]
-
+const dataFetched = (data) => ({
+	type: 'DATA_FETCHED',
+	payload: data,
+});
 
 const ResultPage = () => {
+
 	const location = useLocation();
 	const navigate = useNavigate();
-	const {searchValues} = location.state || {};
+	const { searchValues } = location.state || {};
 
 	useEffect(() => {
 		if (!location.state || !location.state.searchValues) {
@@ -125,18 +57,54 @@ const ResultPage = () => {
 	}, [location.state]);
 
 	const classes = useStyles();
+
+	const dispatch = useDispatch();
+	const data = useSelector(({ exampleReducer }) => exampleReducer.data);
+	const { fetchDataAndProcess } = useSportData(searchValues);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await fetchDataAndProcess();
+			dispatch(dataFetched(result));
+		};
+
+		fetchData();
+	}, []);
+
+	const [activeTab, setActiveTab] = useState(0);
+
+	const handleTabChange = (e, newValue) => {
+		setActiveTab(newValue);
+	};
+
 	return (
 		<div className={classes.root}>
-			{/*<code>{JSON.stringify(searchValues)}</code>*/}
 
 			<div className={classes.wrapper}>
 				<div className={classes.search}>
-					<TranslucentBox component={SearchForm} simplified={true}/>
+					<TranslucentBox component={SearchForm} simplified={true} />
 				</div>
-				
-				<div className={classes.forecast}>
-					<Forecast data={dailyData}/>
-				</div>
+
+				{data && (
+					<div className={classes.forecast}>
+						<Forecast data={data} />
+					</div>
+				)}
+
+				{data && (
+					<div className={classes.graphs}>
+						<h2>10 day forecast</h2>
+						<Tabs value={activeTab} onChange={handleTabChange}>
+							<Tab label={translate('temperature')} />
+							<Tab label={translate('precipitation')} />
+							<Tab label={translate('windSpeed')} />
+						</Tabs>
+
+						{activeTab === 0 && <LineGraph data={data} weatherProperty="temperature" label={translate('temperature') + ' °C'} color="#FF8513" />}
+						{activeTab === 1 && <BarGraph data={data} weatherProperty="precipitation" label={translate('precipitation') + ' mm'} />}
+						{activeTab === 2 && <LineGraph data={data} weatherProperty="wind" label={translate('windSpeed') + ' m/s'} color="#31BD00" />}
+					</div>
+				)}
 			</div>
 		</div>
 	);
